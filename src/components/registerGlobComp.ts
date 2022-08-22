42b9748b341ebcfd346fcdd844ba54635a1d577a
silianpan <liu.pan@silianpan.cn>
2022-08-22 13:15:49 +0800

    fix: 去掉SDrawer

--- src/components/registerGlobComp.ts
+++ src/components/registerGlobComp.ts
@@ -1,15 +1,7 @@
 import type { App } from 'vue';
-import { store } from '/@/store';
-import { router } from '/@/router';
 import { Button } from './Button';
-import { Input, Layout, Modal, Drawer } from 'ant-design-vue';
-import SDrawer from '/@/components2/Drawer/index';
+import { Input, Layout } from 'ant-design-vue';
 
 export function registerGlobComp(app: App) {
-  app.use(Input).use(Button).use(Layout).use(SDrawer, {
-    antdModal: Modal,
-    antdDrawer: Drawer,
-    store,
-    router,
-  });
+  app.use(Input).use(Button).use(Layout);
 }
