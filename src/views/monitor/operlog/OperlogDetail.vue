9c360af109cb92fa87588dd0342b42b5a7d5c158
silianpan <liu.pan@silianpan.cn>
2022-11-16 17:54:03 +0800

    fix: 详情列宽度及布局

--- src/views/monitor/operlog/OperlogDetail.vue
+++ src/views/monitor/operlog/OperlogDetail.vue
@@ -2,18 +2,20 @@
   <Description
     size="middle"
     title="操作信息"
-    :bordered="false"
-    :column="3"
+    bordered
+    :column="1"
     :data="$props.record"
     :schema="operSchema"
+    :labelStyle="{ width: '20%' }"
   />
   <Description
     size="middle"
     title="接口信息"
-    :bordered="false"
-    :column="3"
+    bordered
+    :column="1"
     :data="$props.record"
     :schema="interfaceSchema"
+    :labelStyle="{ width: '20%' }"
   />
 </template>
 <script lang="tsx">
