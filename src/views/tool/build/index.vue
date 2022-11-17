f68d9f88a2dca2160a665f9b564b13a50249a631
silianpan <liu.pan@silianpan.cn>
2022-11-17 14:48:00 +0800

    feat: 构建和swagger

---
+++ src/views/tool/build/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <Result status="success" title="程序员正在加速开发中">
+    <template #extra>
+      <a-button key="home" type="primary" @click="handleHomeClick">返回首页</a-button>
+    </template>
+  </Result>
+</template>
+
+<script setup lang="ts">
+  import { PageEnum } from '/@/enums/pageEnum';
+  import { useGo } from '/@/hooks/web/usePage';
+  import { useUserStore } from '/@/store/modules/user';
+  import { Result } from 'ant-design-vue';
+
+  const userStore = useUserStore();
+  const go = useGo();
+  const handleHomeClick = () => {
+    go(userStore.getUserInfo.homePath || PageEnum.BASE_HOME);
+  };
+</script>
---
+++ src/views/tool/swagger/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <FramePage :frameSrc="src" />
+</template>
+
+<script setup lang="ts">
+  import FramePage from '/@/views/sys/iframe/index.vue';
+  import { getAppEnvConfig } from '/@/utils/env';
+
+  const { VITE_GLOB_API_URL } = getAppEnvConfig();
+  const src = VITE_GLOB_API_URL + '/doc.html';
+</script>
